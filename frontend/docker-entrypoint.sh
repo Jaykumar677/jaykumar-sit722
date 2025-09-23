2417f2aac2a49a80e44f801063d0e033fc4e4c3b
Jaykumar677 <jai.jk739@gmail.com>
2025-09-24 08:20:23 +1000

    fix: updated frontend env handling

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-# Replace placeholders with environment variables and generate config.js
+# Replace env vars in the template and generate config.js
 envsubst < /usr/share/nginx/html/config.template.js > /usr/share/nginx/html/config.js
-
-# Start nginx
-exec nginx -g 'daemon off;'
+exec "$@"
